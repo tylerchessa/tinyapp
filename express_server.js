00fd244235f64eac27e525ed09579bc14ede2c9a
tylerchessa <tylerchessa@gmail.com>
2023-02-21 00:12:11 +0000

    added urls_show.ejs template

--- express_server.js
+++ express_server.js
@@ -9,6 +9,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get('/urls/:id', (req, res) => {
+  const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca"};
+  res.render('urls_show', templateVars);
+}); 
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render('urls_index', templateVars);
