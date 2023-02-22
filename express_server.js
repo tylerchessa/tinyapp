559f5310f674c392555eb0c3932596e2632e143d
tylerchessa <tylerchessa@gmail.com>
2023-02-22 03:31:11 +0000

    can login and saves cookies

--- express_server.js
+++ express_server.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const cookieParser = require('cookie-parser')
 const app = express();
+app.use(cookieParser());
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
@@ -17,7 +19,10 @@ app.get("/", (req, res) => {
 
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies['username']
+  };
   res.render('urls_index', templateVars);
 });
 
@@ -55,7 +60,7 @@ app.post("/urls", (req, res) => {
 
 app.post('/login', (req, res) => {
   const username = req.body.username;
-  res.cookie(username)
+  res.cookie('username', username)
   res.redirect('/urls')
 })
 
